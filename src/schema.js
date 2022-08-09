2631917820df00bdd3bc7393cb97411675d2fac7
Aihaiti Abudureheman <aihaiti.abudureheman@cybercom.com>
2022-08-09 19:06:11 +0300

    Initial Commit

--- src/schema.js
+++ src/schema.js
@@ -6,6 +6,8 @@ const typeDefs = gql`
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
     track(id: ID!): Track
+    "Fetch single module by id"
+    module(id: ID!): Module!
   }
 
   type Mutation {
@@ -51,6 +53,10 @@ const typeDefs = gql`
     title: String!
     "The Module's length in minutes"
     length: Int
+    "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
+    content: String
+    "The module's video url, for video-based modules"
+    videoUrl: String
   }
 
   "Author of a complete Track"
